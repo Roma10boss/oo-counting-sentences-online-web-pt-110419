12f44ec79033cac1c51c9740f509702a1f6db329
Romario Gustave <romagustave10@gmail.com>
2019-12-28 06:32:15 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,46 @@
 require 'pry'
 
 class String
+  attr_accessor :sentence, :question, :exclamation, :count_sentences
 
   def sentence?
+  
+    if self.end_with? (".")
+      return true
+    else
+    return  false
     
   end
+end
 
   def question?
+    
+    if self.end_with? ("?")
+      
+      return true
+    else
+     return false
 
   end
+end
 
   def exclamation?
+    
+    if self.end_with? ("!")
+      return true
+    else
+     return false
 
-  end
+  end 
+end 
 
-  def count_sentences
-
+  def count_sentences()
+     if new_array =[]
+      return 0  
+      else
+    new_array = self.split(/[.?!]/).delete_if{|new_array| new_array.length == 0}.count
+    return count_sentences.length
+   
+    end 
   end
 end
